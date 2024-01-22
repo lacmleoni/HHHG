3614fb9c63a4c6f176d258a22a9b5d2a262071fc
lacmleoni <154819118+lacmleoni@users.noreply.github.com>
2024-01-22 09:42:35 +0100

    upload pointcloud

---
+++ pointclouds/HHHG/cloud.js
@@ -0,0 +1,67 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 21916637,
+    "boundingBox": {
+        "lx": -80.59347534179688,
+        "ly": 6.183889389038086,
+        "lz": -2.955373764038086,
+        "ux": 34.59851837158203,
+        "uy": 121.37588310241699,
+        "uz": 112.23661994934082
+    },
+    "tightBoundingBox": {
+        "lx": -80.5934753,
+        "ly": 6.1838894,
+        "lz": -2.9553738,
+        "ux": 34.598518399999999,
+        "uy": 81.8948822,
+        "uz": 52.8315353
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.9975919127464294,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
